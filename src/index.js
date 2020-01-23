339ffb7bf18d38806167c30d5b6ab84d03002a0b
YeisonTapia <yftapial@ut.edu.co>
2020-01-23 15:57:18 -0500

    + Add function for order data, by a key of array.

--- src/index.js
+++ src/index.js
@@ -38,3 +38,20 @@ export const keysToCamel = (o) => {
 export const isEmpty = (obj) => {
   return Object.keys(obj).length === 0;
 }
+
+export const orderdata = (data, key) => {
+  return data.sort((a,b) => {
+    if(b[key] == null){
+      return -1
+    }
+    let response = 0;
+    let aValue = parseInt(a[key])
+    let bValue = parseInt(b[key])
+    if (aValue > bValue) {
+      response = 1;
+    } else if (aValue < bValue) {
+      response = -1;
+    }
+    return response * -1
+  })
+}
